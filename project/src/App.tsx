465461baf64c2f34d68009d9d4a567e463274f10
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-03 15:11:16 -0300

    fix: fixes the bug of turning the navbar home button selected when reloading the page

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,12 +1,30 @@
+import { useTranslation } from "react-i18next"
 import { useEffect, useState } from "react"
 import AuthContext from "./context/AuthContext"
 import AppRoutes from "./routes/AppRoutes"
 import { AuthData, onLogin, onPageLoad } from "./service/auth.service"
 import { NavbarContext, NavbarType } from "./context/NavbarContext"
 
-
 const App = () => {
+  const { t } = useTranslation()
+
+  const getFromURL = (key: string) => {
+    switch (key) {
+    case t("url.code"):
+      return "code"
+    case t("url.leave"):
+      return "leave"
+    case t("url.register"):
+      return "register"
+    default:
+      return "home"
+    }
+  }
+
+
   const [ auth, setAuth ] = useState<AuthData>({ authenticated: true })
+  
+  const [ selected, setSelected ] = useState<NavbarType>(getFromURL(`/${window.location.pathname.split("/")[1]}`))
 
   useEffect(() => {
     onPageLoad()
@@ -21,8 +39,6 @@ const App = () => {
         setAuth(data)
       })
   }
-
-  const [ selected, setSelected ] = useState<NavbarType>("home")
 
   return (
     <AuthContext.Provider value={{ auth, handleOnLogin }}>
