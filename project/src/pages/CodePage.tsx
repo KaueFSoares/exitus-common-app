ac6965b7088c6e03cbdd42f9b5f4c280f06e389e
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-04 19:53:19 -0300

    feat: blocks the code page from guardian and the leaves page from employees and guardeds

--- project/src/pages/CodePage.tsx
+++ project/src/pages/CodePage.tsx
@@ -4,6 +4,8 @@ import { useTranslation } from "react-i18next"
 import PageWrapper from "../components/wrapper/PageWrapper"
 import { onLoad, onUpdate } from "../service/code.service"
 import AuthContext from "../context/AuthContext"
+import { RoleType } from "../enum/RoleType"
+import { NotFoundPage } from "."
 
 const CodePage = () => {
   const { t } = useTranslation()
@@ -26,7 +28,7 @@ const CodePage = () => {
       })
   }
 
-  return (
+  return (auth.role === RoleType.EMPLOYEE || auth.role === RoleType.GUARDED) ? (
     <PageWrapper>
       <section className="w-full h-full flex flex-col items-center p-5 xsm:p-10">
         <div className="w-full flex flex-col justify-center mb-4 xsm:mb-8">
@@ -50,7 +52,7 @@ const CodePage = () => {
         </button>
       </section>
     </PageWrapper>
-  )
+  ) : <NotFoundPage />
 }
 
 export default CodePage
--- project/src/pages/LeavePage.tsx
+++ project/src/pages/LeavePage.tsx
@@ -1,11 +1,17 @@
+import { useContext } from "react"
 import PageWrapper from "../components/wrapper/PageWrapper"
+import AuthContext from "../context/AuthContext"
+import { RoleType } from "../enum/RoleType"
+import { NotFoundPage } from "."
 
 const LeavePage = () => {
-  return (
+  const { auth } = useContext(AuthContext)
+
+  return (auth.role === RoleType.GUARDIAN) ? (
     <PageWrapper>
       <div>LeavePage</div>
     </PageWrapper>
-  )
+  ) : <NotFoundPage />
 }
 
 export default LeavePage
