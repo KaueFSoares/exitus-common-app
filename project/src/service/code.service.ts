c7874895ec288aada831e6125d2a90c1fcd2208f
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-04 09:36:55 -0300

    feat: creates the code page service

---
+++ project/src/service/code.service.ts
@@ -0,0 +1,40 @@
+import { ClosedAPI } from "./api"
+
+/**
+ * The `onLoad` function makes an API request to retrieve a code fingerprint and returns it as a
+ * string.
+ * @param {string} token - The `token` parameter is a string that represents the authentication token
+ * required to access the API.
+ * @returns {Promise<string>} a Promise that resolves to a string.
+ */
+export const onLoad = async (token: string): Promise<string> => {
+  const response = await ClosedAPI(token).get("/code.json")
+
+  if (response.status !== 200) {
+    throw new Error("Load failed")
+  }
+
+  const data = response.data.fingerprint as string
+
+  return data
+}
+
+/**
+ * The function `onUpdate` makes an asynchronous request to update code using a token and returns the
+ * fingerprint data.
+ * @param {string} token - The `token` parameter is a string that represents the authentication token
+ * required to access the ClosedAPI. It is used to authenticate the user and authorize the update
+ * request.
+ * @returns {Promise<string>} a Promise that resolves to a string.
+ */
+export const onUpdate = async (token: string): Promise<string> => {
+  const response = await ClosedAPI(token).put("/updateCode.json")
+
+  if (response.status !== 200) {
+    throw new Error("Update failed")
+  }
+
+  const data = response.data.fingerprint as string
+
+  return data
+}
