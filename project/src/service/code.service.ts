df3171c46a31bb168038dc902bb255ff1efbbf4d
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-04 21:05:08 -0300

    feat: saves the fingerprint on the localstorage

--- project/src/service/code.service.ts
+++ project/src/service/code.service.ts
@@ -7,15 +7,17 @@ import { URL } from "./url"
  * @returns {Promise<string>} a Promise that resolves to a string  to be showed as a QR Code.
  */
 export const onLoad = async (token: string): Promise<string> => {
-  const response = await ClosedAPI(token).get(URL.CODE)
+  const data = getFromLocalStorage()
 
-  if (response.status !== 200) {
-    throw new Error("Load failed")
+  if (data) {
+    return data
   }
 
-  const data = response.data.fingerprint as string
+  const response = await getFromApi(token)
 
-  return data
+  saveInLocalStorage(response)
+
+  return response
 }
 
 /**
@@ -28,6 +30,44 @@ export const onUpdate = async (token: string): Promise<string> => {
 
   if (response.status !== 200) {
     throw new Error("Update failed")
+  }
+
+  const data = response.data.fingerprint as string
+
+  saveInLocalStorage(data)
+
+  return data
+}
+
+/**
+ * Retrieves the value of the "token" key from the browser's local storage.
+ * @returns {string | null} The function `getFromLocalStorage` is returning a value of type `string` or `null`.
+ */
+const getFromLocalStorage = (): string | null => {
+  return localStorage.getItem("fingerprint")
+}
+
+/**
+ * Stores the value of the "fingerPrint" key in the browser's local storage.
+ * @param {string} fingerPrint - the value to be stored in the browser's local storage.
+ * @returns {void} The function `setInLocalStorage` is returning a value of type `void`.
+ */
+const saveInLocalStorage = (fingerPrint: string): void => {
+  localStorage.setItem("fingerprint", fingerPrint)
+}
+
+/**
+ * The function `getFromApi` makes an API call to retrieve a fingerprint data using a token and returns
+ * the data as a promise.
+ * @param {string} token - The `token` parameter is a string that represents the authentication token
+ * required to access the API.
+ * @returns {Promise<string>} a Promise that resolves to a string.
+ */
+const getFromApi = async (token: string): Promise<string> => {
+  const response = await ClosedAPI(token).get(URL.CODE)
+
+  if (response.status !== 200) {
+    throw new Error("Load failed")
   }
 
   const data = response.data.fingerprint as string
