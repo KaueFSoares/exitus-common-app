76485f9d434d00fe36e0cbb5d4bc7a6af1bce068
Kauê Soares <kaue222120@gmail.com>
2023-11-21 23:03:59 -0300

    refactor: removes temporary unused url's

--- project/src/service/api/url.ts
+++ project/src/service/api/url.ts
@@ -1,10 +1,6 @@
 export enum URL {
-  BASE = "http://localhost:8080/api",
-  LOGIN = "/auth/login",
-  REFRESH_ACCESS_TOKEN = "/refresh-token",
-  CODE = "/user/code",
-  UPDATE_CODE = "/user/update-code",
+  BASE = "http://localhost:3000",
+  LOGIN = "/auth",
+  REFRESH_ACCESS_TOKEN = "/auth/refresh-token",
 
-
-  REGISTER = "/register.json",
 }
