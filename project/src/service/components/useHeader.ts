c70ef4eb0472ba058402e28d078df6ac83bd46f8
Kauê Soares <kaue222120@gmail.com>
2023-11-22 18:43:32 -0300

    feat: creates the hook for loading data for the app header

---
+++ project/src/service/components/useHeader.ts
@@ -0,0 +1,20 @@
+import useApi from "../api/api"
+import { URL } from "../api/url"
+
+const useHeader = () => {
+  const api = useApi()
+
+  const getName = async () => {
+    const response = await api.get(URL.USER)
+    
+    const data = await response.data.name as string
+
+    return data
+  }
+
+  return {
+    getName: getName,
+  }
+}
+
+export default useHeader
