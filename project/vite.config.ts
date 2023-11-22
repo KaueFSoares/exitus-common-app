0bb96b498ec17e71a564588d4ca5199b515106b8
Kauê Soares <kaue222120@gmail.com>
2023-11-22 15:53:25 -0300

    fix: fixes vite config file

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,18 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "white": "#ffffff",
-        "dark-green": "#00420C",
-        "light-green": "#17882C",
-        "red": "#C90C0F",
-      },
-    },
-  },
-  plugins: [],
-}
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [ react() ],
+})
