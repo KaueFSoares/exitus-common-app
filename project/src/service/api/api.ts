144809572d234270346568cfa89cb352c9a3e012
Kauê Soares <kaue222120@gmail.com>
2023-11-21 22:55:48 -0300

    feat: implements useApi hook with refresh token interceptor

--- project/src/service/api/api.ts
+++ project/src/service/api/api.ts
@@ -1,20 +1,54 @@
-import axios, { AxiosInstance } from "axios"
-import { URL } from "./url"
+import axios, { AxiosInstance } from 'axios'
+import { useContext } from 'react'
+import AuthContext from '../../context/AuthContext'
+import { AuthData, onLogout, onRefresh } from '../auth.service'
+import { URL } from './url'
+import { jwtDecode } from 'jwt-decode'
 
 /**
- * Creates a new Axios instance with the provided token.
- *
- * @param {string} token - The token to be used in the Authorization header.
- * @returns {AxiosInstance} A Axios instance with the provided token.
- * @throws {Error} If there's an error during process.
+ * Returns an instance of Axios configured for making API requests.
+ * @returns {AxiosInstance} The configured Axios instance.
  */
-export const ClosedAPI = (token: string): AxiosInstance => {
-  return axios.create({
+const useApi = (): AxiosInstance => {
+  const { setAuthenticated } = useContext(AuthContext)
+
+  const authData = JSON.parse(localStorage.getItem("authData") as string) as AuthData
+
+  const decodedJWT = jwtDecode(authData.access_token)
+
+  const instance = axios.create({
     baseURL: URL.BASE,
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
-      Authorization: `Bearer ${token}`,
     },
   })
+
+  instance.interceptors.request.use(async (req) => {
+    console.log(decodedJWT.exp!)
+    const isExpired = new Date(decodedJWT.exp!) < new Date()
+
+    if (!isExpired){
+      req.headers.Authorization = `${authData.access_token}`
+      
+      return req
+    } 
+
+    await onRefresh(authData.refresh_token.id)
+      .then((res) => {
+        req.headers.Authorization = `${res.access_token}`
+      })
+      .catch((err) => {
+      // eslint-disable-next-line no-console
+        console.log(err)
+        onLogout()
+        setAuthenticated(false)
+      })
+
+    return req
+  })
+  
+  return instance
 }
+
+export default useApi
