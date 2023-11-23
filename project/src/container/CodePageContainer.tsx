d263da32048db5bf511109663d6088297ba5bd46
Kauê Soares <kaue222120@gmail.com>
2023-11-22 21:06:59 -0300

    feat: implements api calling

--- project/src/container/CodePageContainer.tsx
+++ project/src/container/CodePageContainer.tsx
@@ -1,11 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { CodePage } from "../pages"
+import useCode from "../service/pages/code.service"
 
 const CodePageContainer = () => {
   const [ code, setCode ] = useState<string>("")
 
+  const codeService = useCode()
+
+  useEffect(() => {
+    codeService.getCode().then((res) => {
+      setCode(res)
+    })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   const handleOnUpdate = () => {
-    setCode("new code")
+    codeService.updateCode().then((res) => {
+      setCode(res)
+    })
   }
 
   return (
