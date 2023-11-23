e4ffb1c17dddf7c2ae32a7ee3dd933a08ffb40cd
Kauê Soares <kaue222120@gmail.com>
2023-11-22 21:44:37 -0300

    feat: implements loading screen on code page

--- project/src/container/CodePageContainer.tsx
+++ project/src/container/CodePageContainer.tsx
@@ -1,22 +1,31 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { CodePage } from "../pages"
 import useCode from "../service/pages/code.service"
+import LoadingContext from "../context/LoadingContext"
 
 const CodePageContainer = () => {
   const [ code, setCode ] = useState<string>("")
 
   const codeService = useCode()
 
+  const { setLoading } = useContext(LoadingContext)
+
   useEffect(() => {
+    setLoading(true)
+
     codeService.getCode().then((res) => {
       setCode(res)
+      setLoading(false)
     })
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const handleOnUpdate = () => {
+    setLoading(true)
+
     codeService.updateCode().then((res) => {
       setCode(res)
+      setLoading(false)
     })
   }
 
