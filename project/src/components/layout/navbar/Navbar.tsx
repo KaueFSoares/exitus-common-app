5e346ed7df90558d459c798a6e6fad011157ceb7
KaueFSoares <kaue222120@gmail.com>
2023-11-28 14:35:13 -0300

    deploy: removes unused code that was breaking deploy version

--- project/src/components/layout/navbar/Navbar.tsx
+++ project/src/components/layout/navbar/Navbar.tsx
@@ -3,30 +3,17 @@ import { BsQrCode } from "react-icons/bs"
 import { IoDocumentTextOutline } from "react-icons/io5"
 import { useTranslation } from "react-i18next"
 import { useContext } from "react"
-import { RxExit } from "react-icons/rx"
-import AuthContext from "../../../context/AuthContext"
-import { RoleType } from "../../../enum/RoleType"
 import { NavbarContext } from "../../../context/NavbarContext"
 import NavbarItem from "./NavbarItem"
 
 const Navbar = () => {
   const { t } = useTranslation()
 
-  const { authenticated } = useContext(AuthContext)
-
   const { selected, setSelected } = useContext(NavbarContext)
 
   return (
     <nav className="shadow-full flex py-4 px-8 gap-8 rounded-full fixed bottom-4 xsm:bottom-8 bg-white z-40">
-      {RoleType.GUARDED === RoleType.GUARDIAN ? (
-        <NavbarItem
-          url={t("url.leave")}
-          type = "leave"
-          icon={<RxExit />}
-          selected={selected === "leave"}
-          handleOnClick={setSelected}
-        />
-      ) : (
+      {        
         <NavbarItem
           url={t("url.code")}
           type = "code"
@@ -34,7 +21,7 @@ const Navbar = () => {
           selected={selected === "code"}
           handleOnClick={setSelected}
         />
-      )}
+      }
 
       <NavbarItem
         url={t("url.home")}
--- project/src/pages/LeavePage.tsx
+++ project/src/pages/LeavePage.tsx
@@ -1,17 +1,7 @@
-import { useContext } from "react"
-import PageWrapper from "../components/wrapper/PageWrapper"
-import AuthContext from "../context/AuthContext"
-import { RoleType } from "../enum/RoleType"
 import { NotFoundPage } from "."
 
 const LeavePage = () => {
-  const { auth } = useContext(AuthContext)
-
-  return (auth.role === RoleType.GUARDIAN) ? (
-    <PageWrapper>
-      <div>LeavePage</div>
-    </PageWrapper>
-  ) : <NotFoundPage />
+  return <NotFoundPage />
 }
 
 export default LeavePage
