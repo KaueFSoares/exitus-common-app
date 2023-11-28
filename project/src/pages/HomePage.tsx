cdb07d98db16ce274ccf219539c415b70d0efa58
KaueFSoares <kaue222120@gmail.com>
2023-11-28 14:26:08 -0300

    deploy: implementes .env file and importing for deploy

--- project/src/pages/HomePage.tsx
+++ project/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import PageWrapper from "../components/wrapper/PageWrapper"
 
 const HomePage = () => {
   return (
-    <PageWrapper>
+    <PageWrapper full>
       <h1>Home Page</h1>
     </PageWrapper>
   )
--- project/src/service/api/url.ts
+++ project/src/service/api/url.ts
@@ -1,9 +1,9 @@
-export enum URL {
-  BASE = "http://localhost:3000",
-  LOGIN = "/auth",
-  REFRESH_ACCESS_TOKEN = "/auth/refresh-token",
-  MY_PROFILE = "/my-profile",
-  MY_CODE = "/my-code",
-  REFRESH_MY_CODE = "/refresh-my-code",
-  MY_REGISTERS = "/my-registers",
+export const URL = {
+  BASE: `${import.meta.env.VITE_API}`,
+  LOGIN: "/auth",
+  REFRESH_ACCESS_TOKEN: "/auth/refresh-token",
+  MY_PROFILE: "/my-profile",
+  MY_CODE: "/my-code",
+  REFRESH_MY_CODE: "/refresh-my-code",
+  MY_REGISTERS: "/my-registers",
 }
