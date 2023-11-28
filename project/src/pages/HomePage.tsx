55831c431254d7c74255633736d62b7df9955f93
KaueFSoares <kaue222120@gmail.com>
2023-11-28 14:28:18 -0300

    feat: adds ifsul logo on home page

--- project/src/pages/HomePage.tsx
+++ project/src/pages/HomePage.tsx
@@ -3,7 +3,9 @@ import PageWrapper from "../components/wrapper/PageWrapper"
 const HomePage = () => {
   return (
     <PageWrapper full>
-      <h1>Home Page</h1>
+      <div className="flex items-center justify-center">
+        <img src="/images/IfsulVericalLogo.png" alt="Ifsul Logo" className="opacity-50 md:w-1/2" />
+      </div>
     </PageWrapper>
   )
 }
